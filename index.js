92443ba61fb1cae60e0ac688098a1cba39415c9d
Luigi-Teodoro <luigi.tc14@gmail.com>
2025-03-04 17:08:51 -0500

    actu

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server is running http://localhost:3000');
+});
